8f01d8ecaf84dbe1db9133c1b17888d3a2f5191f
Mike <nguyen.mike1092@gmail.com>
2024-01-12 12:59:01 -0500

    added btn

--- assets/js/script.js
+++ assets/js/script.js
@@ -18,7 +18,7 @@ const getAnimeTrack = async (anime) => {
 		if (songs.length !== 0) {
 			//FIXME: make sure to change to `songs` out when done
 			songs.forEach((element) => {
-				const card = $(`<button type="button">${element.title} </button>`);
+				const card = $(`<button type="button" class="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700">${element.title} </button>`);
 				card.on("click", () => {
 					addTrackToLocal(element);
 					window.location.href = "index2.html";
