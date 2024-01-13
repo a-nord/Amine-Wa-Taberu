7a18ce41bdae52468ee2ac9ae16ebb299237dbdf
Mike <nguyen.mike1092@gmail.com>
2024-01-12 20:14:35 -0500

    makeing comments and changing variables names

--- assets/js/artist.js
+++ assets/js/artist.js
@@ -1,81 +1,89 @@
-const artistInfo = $("#artist-info");
-const trackName = $("#trackName");
-const artistBio = $("#artist-bio");
-const songList = $(".song-list");
+//================= DOM Traversal =================//
+const artistNameEl = $(".artist-name");
+const trackNameEl = $("#track-name");
+const artistBioEl = $("#artist-bio");
+const songListEl = $(".song-list");
 
+//================ Global Variables ===============//
 const apiKey = `eeb927aca555bdd1797a9ff27182de7f`;
 const apiSecret = `09f025e237eebbd253e8eadfc9b9edfd`;
 
-const getTrackInfo = async (track) => {
+//=================== Functions ==================//
+
+//=== Get the artist from the song's name ===//
+const getTrackInfo = async (songName) => {
 	console.log(1);
 	try {
-		const lastFmAPI = `https://ws.audioscrobbler.com/2.0/?method=track.search&track=${track}&api_key=${apiKey}&format=json`;
+		const lastFmAPI = `https://ws.audioscrobbler.com/2.0/?method=track.search&track=${songName}&api_key=${apiKey}&format=json`;
 
 		const trackResponse = await fetch(lastFmAPI);
 		const trackData = await trackResponse.json();
 
-		const firstTrack = trackData.results?.trackmatches?.track[0];
-		const artistName = firstTrack.artist;
+		// Traverse though the data to get the artist's name
+		const artistName = trackData.results.trackmatches.track[0].artist;
 
-		console.log(artistName);
-
-		console.log(trackData);
-
+		// If there is a name then run these functions
 		if (artistName) {
 			getArtistBio(artistName);
 			getArtistSongs(artistName);
 
-			trackName.append(track);
+			// Put the song's name on the page
+			trackNameEl.append(songName);
 		} else {
 			console.log("No artist found");
 		}
 	} catch (error) {
 		console.error("Errrr", error.message);
 	}
-};
+}; // END
 
+//=== Get artist's biography from API ===//
 const getArtistBio = async (artistName) => {
 	try {
 		const lastFmArtist = `https://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist=${artistName}&api_key=${apiKey}&format=json`;
+
 		const artistResponse = await fetch(lastFmArtist);
 		const artistData = await artistResponse.json();
-		console.log(artistData);
 
 		const content = artistData.artist.bio.content;
-		const formattedContent = content.replace(/\n/g, '<br>'); // Replace newline characters with HTML line break tags
 
-        artistBio.append(formattedContent); // Use innerHTML to render HTML content
-        console.log(formattedContent);
+		// Replace newline characters with HTML line break tags
+		const formattedContent = content.replace(/\n/g, "<br>"); 
 
-		artistInfo.append(`by ` + artistName);
+		artistBioEl.append(formattedContent);
+		artistNameEl.append(artistName);
+
 	} catch (error) {
 		console.error("Errrr", error.message);
 	}
-};
+}; // END
 
+//=== Get the artist's 5 top songs ===//
 const getArtistSongs = async (artistName) => {
 	try {
 		const lastFmTopSongs = `https://ws.audioscrobbler.com/2.0/?method=artist.gettoptracks&artist=${artistName}&api_key=${apiKey}&format=json`;
+
 		const topSongsResponse = await fetch(lastFmTopSongs);
 		const topSongsData = await topSongsResponse.json();
-		console.log(topSongsData.toptracks.track);
 
+		//Just get the first 5 index
 		const topSongs = topSongsData.toptracks.track.slice(0, 5);
-		topSongs.forEach((element) => {
-			const songName = $(`<li>${element.name}</li>`);
-			songList.append(songName);
+		topSongs.forEach((topSongs) => {
+			const songName = $(`<li>${topSongs.name}</li>`);
+			songListEl.append(songName);
 		});
 
 		// console.log(typeof topSongsData.toptracks.track);
 	} catch (error) {
 		console.error("Errrr", error.message);
 	}
-};
+}; // END
 
+//=== Get song name from local storage ===//
 const getFromLocal = () => {
 	let getTrack = JSON.parse(localStorage.getItem("song"));
 	console.log(getTrack);
 	getTrackInfo(getTrack);
 };
-
+// init function
 getFromLocal();
